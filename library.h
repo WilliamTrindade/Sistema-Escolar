e9e44462a1ab48a74b6baa990b76abec7af57279
William Trindade <williamtrindade777@gmail.com>
2018-06-21 16:10:14 -0300

    Update library.h

--- library.h
+++ library.h
@@ -1,10 +1,10 @@
-//DECLARA«√O DE VARIAVEIS
+//DECLARA√á√ÉO DE VARIAVEIS
 #define TAM 10
-int opt = 0;
+int opt = 0, m=0, email_valido = 0, matricula_valida = 0;
 
 //ESTRUTURA DOS ALUNOS
 struct x{
-	char nome[20], sobrenome[20], email[50];
+	char nome[50], sobrenome[20], email[50];
 	int matricula, disciplinas[5], grade[4][5];
 };
 typedef struct x alun;
@@ -20,19 +20,55 @@ typedef struct y disc;
 disc disciplinas[5];
 
 //ASSOCIAR ALUNO A DISCIPLINA
-void associar_aluno(){
-	printf("as");
-	opt = 0;
-	printf("\n\n");
-	system("pause");
-	system("cls");
-}
 
 void inserir_aluno(){
-	printf("as");
+	//se O NUMERO DE ALUNOS MATRICULADOS FOR MAIOR QUE tam(10)
+	if(m < TAM){
+		char x[50];
+		int sair;
+		printf("\tCadastro de Aluno em Disciplinas\n\nMatricula %d.\n\n", m);
+		alunos[m].matricula = m;
+		
+		printf("Digite o Nome do Aluno: \n\n");
+		fflush(stdin);
+		gets(alunos[m].nome);
+		
+		printf("\n\nDigite o Sobrenome do Aluno:\n\n");
+		fflush(stdin);
+		gets(alunos[m].sobrenome);
+		
+		while(email_valido == 0){
+			printf("\n\nDigite o  E-mail do Aluno:\n\n");
+			fflush(stdin);
+			gets(alunos[m].email);
+			email_valido = valida_email(alunos[m].email);
+		}
+		while(matricula_valida == 0 && sair == 0){
+			printf("\n\n\tQuais disciplinas %s %s vai cursar?\n\n\nDigite o codigo ou o nome:\n", alunos[m].nome, alunos[m].sobrenome);
+			gets(x);
+			matricula_valida = valida_matricula(x);
+			if(matricula_valida == 1){
+				printf("\nQuer matricula-lo em mais disciplinas? sim = 1 / nao = 0\n\n");
+				scanf("%d", &sair);
+			}
+		}
+		
+		v
+		m++;
+		printf("\n\n");
+	}else{
+		printf("\n\n voce ja cadastrou o numero maximo de alunos\n\n");
+	}
+	
 	opt = 0;
-	printf("\n\n");
 	system("pause");
 	system("cls");
 	
+}
+int valida_email(){
+	
+	return 1;	
+}
+char valida_matricula{
+
 }
